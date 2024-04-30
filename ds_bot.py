f48b5e76b0d295e3733cca86aa54de18181f3b23
DemonMix <cvgfcvgfyt@gmail.com>
2024-05-01 00:13:32 +0400

    Ds

--- ds_bot.py
+++ ds_bot.py
@@ -7,7 +7,6 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -28,6 +27,29 @@ async def ds_get_wiki_summary(ctx, *, query=''):
         return
     result = get_wiki_summary(query)
     await ctx.send(result)
+
+
+@bot.command(name='dice')
+async def ds_handle_dice_roll(ctx, dice=''):
+    if not dice:
+        await ctx.send('Отправьте сообщение в формате: !dice <кол-во костей>x<кол-во граней>')
+        return
+    result = handle_dice_roll(dice)
+    if result:
+        await ctx.send(result)
+
+
+@bot.command(name='cat')
+async def ds_get_random_cat_pic(ctx):
+    result = get_random_cat_pic()
+    await ctx.send(result)
+
+
+@bot.command(name='dog')
+async def ds_get_random_dog_pic(ctx):
+    result = get_random_dog_pic()
+    await ctx.send(result)
+
 
 if __name__ == '__main__':
     bot.run(DS_TOKEN)
